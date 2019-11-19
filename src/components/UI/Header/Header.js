33e815d282f07a0641ecc0060aaf58ddc0109f35
Daniel Waters <danwater1@gmail.com>
2019-11-18 21:51:09 -0800

    Fixes color theme to defined set so easy to change theme

--- src/components/UI/Header/Header.js
+++ src/components/UI/Header/Header.js
@@ -1,16 +1,12 @@
 import React from "react";
 
-import Logo from "../Logo/Logo";
 import Button from "../Button/Button";
 
 import classes from "./Header.module.scss";
 
 const Header = props => (
   <div className={classes.Header}>
-    <div className={classes.logo}>
-      <Logo color="white" />
-    </div>
-    <div className={classes.logoText}>NTHOLOGY</div>
+    <div className={classes.logoText}>ANTHOLOGY</div>
     <Button clicked={props.clicked} btnType="primary">
       Add Book
     </Button>
